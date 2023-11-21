cda7da41dceb42ace695d2a347ba964a1329e992
Kingsley Maina <mainakingsley7@gmail.com>
2023-11-21 14:53:43 +0300

    Completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,42 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    i = 10
+    while i > 0 :
+        print(i)
+        i -= 1
+    print("Happy New Year")  
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    # code goes here
+    new = list()
+    for num in int_list :
+        new.append(num ** 2)
+        print(new)
 
-def fizzbuzz():
+print(square_integers([1,2,3,4,5]))        
+
+def fizzbuzz(num):
     # code goes here!
-    pass
+    if num % 3 == 0 and num % 5 == 0 :
+        return "FizzBuzz";
+    elif num % 3 == 0 :
+        return "Fizz";
+    elif num % 5 == 0 :
+        return "Buzz";
+    else :
+        return num;
+
+def fizzbuzz_printer():
+    num = 1 
+    while num <= 100 :
+        print(fizzbuzz(num))
+        num += 1
+
+def reverse_string(str):
+    new_string =""
+    for x in range(len(str)-1,-1,-1) :
+            new_string += str[x]
+    return new_string
+
+
